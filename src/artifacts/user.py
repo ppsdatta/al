7cb48e61b483af1893c24affe31fbfc90c37ddef
Sourav Datta <sdatta@planview.com>
2016-11-17 19:35:56 +0530

    Add profile message

--- src/artifacts/user.py
+++ src/artifacts/user.py
@@ -6,21 +6,39 @@
 from globals import SESSION
 from auth import ALRequester
 
+
 SLAM_RESOURCE = 'api/v1/user/{0}/slam'
+PROFILE_RESOURCE = 'api/v1/user/{0}/profile'
 
 
-def get_slam():
+def get_resource(resource):
     sess = SESSION['session']
     uid = SESSION['uid']
     if sess is None:
         return ''
-    return ALRequester.request(SLAM_RESOURCE.format(uid))
+    return ALRequester.request(resource.format(uid))
+
+
+def get_slam():
+    return get_resource(SLAM_RESOURCE)
+
+
+def get_profile():
+    return get_resource(PROFILE_RESOURCE)
 
 
 def greet_message():
     message = ''
 
     # here goes user info getting part (first name etc.)
+    profile = get_profile()
+    profile_message = ''
+    if profile[0]:
+        user_profile = profile[1].json()
+        SESSION['user_profile'] = user_profile
+
+        profile_message = 'Hi, {}!'.format(user_profile['first_name']) + '\n'
+        message += profile_message
 
     # here we get slam status
     slam_status = get_slam()
