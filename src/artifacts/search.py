75b2ca17e4b2493901d184f91b7ff60272d245ae
Sourav Datta <sdatta@planview.com>
2016-11-18 15:25:49 +0530

    Add search and bash script to start Al

---
+++ src/artifacts/search.py
@@ -0,0 +1,16 @@
+"""
+
+
+"""
+
+from globals import SESSION
+from artifacts import get_search_stuffs
+
+
+
+SEARCH_RESOURCE = 'api/v2/search?q={}'
+
+
+def search(query):
+    return get_search_stuffs(SEARCH_RESOURCE, query)
+
